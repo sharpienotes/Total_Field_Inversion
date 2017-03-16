ed2d01bb3e40c9063f2e59d04ddbda7082916fc8
sharpienotes <sharpienotes@users.noreply.github.com>
2017-03-16 17:50:59 +0100

    playing with base for gauss-newton

--- argmin_playing.py
+++ argmin_playing.py
@@ -1,33 +1,66 @@
-# this file is used to play with the argmin function and get a grasp on it
-
-import numpy
+import numpy as np
 import scipy
-from scipy import optimize
 
-#numbers = numpy.random.random([3,3,3,5,4])
-#values = numpy.arange(15).reshape(3, 5)
-#values = numpy.random.random([3,3,3,5,4])
-values = numpy.array([10, 9, 4, 9, 4, 1, 2, 3, 4, 5, 3, 3]).reshape(3,4)
-#print(values)
-minmin = numpy.ndarray.argmin(values)
-#print('Good Afternoon, the minimum is at: '+str(minmin))
-squirrel = numpy.unravel_index(minmin, values.shape)
-#print(squirrel)
+import pymrt.utils as pmu
+import pymrt.geometry as pmg
 
-#output is:
-#[[10  9  4  9]
-# [ 4  1  2  3]
-# [ 4  5  3  3]]
-#Good Afternoon, the minimum is at: 5
-#(1, 1)
+from pymrt.constants import GAMMA, GAMMA_BAR
 
-badger = numpy.array([1,2,3,4,5,6,7,8,9,10,11,12]).reshape(3,4)
-# unravel translates the number of the coordinate to an actual shape in space for this array
-#a = scipy.optimize.nnls(squirrel)
+def dipole_kernel(
+        shape,
+        origin=0.5,
+        theta=0.0,
+        phi=0.0):
+    """
+    Generate the 3D dipole kernel in the Fourier domain.
 
-#scipy.optimize.minimize(method=’Newton-CG’)
-# todo: test with sin(x) and derivative cos(x)
-# might not work with pure arrays like that but rather an actual dependence and derivative relation
-badger_prime = numpy.array([1,2,3,4,5,6,7,8,9,10,11,12]).reshape(3,4)
-#print(badger_prime)
-Newton = scipy.optimize.fmin_ncg(badger,[1,1],badger_prime) # nope, does not work like that!
+    .. math::
+
+        C=\\frac{1}{3}-\\frac{k_z \\cos(\\theta)\\cos(\\phi)
+        -k_y\\sin(\\theta) \\cos(\\phi)+k_x\\sin(\\phi))^2}
+        {k_x^2 + k_y^2 + k_z^2}
+
+    Args:
+        shape (tuple[int]): 3D-shape of the dipole kernel array.
+            If not a 3D array, the function fails.
+        origin (float|tuple[float]): Relative position of the origin.
+            Values are in the [0, 1] interval.
+        theta (int|float): Angle of 1st rotation (along x-axis) in deg.
+            Equivalent to the projection in the yz-plane of the angle between
+            the main magnetic field B0 (i.e. the principal axis of the dipole
+            kernel) and the z-axis. If phi is 0, the projection simplifies to
+            the identity.
+        phi (int|float): Angle of 2nd rotation (along y-axis) in deg.
+            Equivalent to the projection in the xz-plane of the angle between
+            the main magnetic field B0 (i.e. the principal axis of the dipole
+            kernel) and the z-axis. If theta is 0, the projection simplifies to
+            the identity.
+
+    Returns:
+        dk_arr (np.ndarray): The dipole kernel in the Fourier domain.
+    """
+    #     / 1   (kz cos(th) cos(ph) - ky sin(th) cos(ph) + kx sin(ph))^2 \
+    # C = | - - -------------------------------------------------------- |
+    #     \ 3                      kx^2 + ky^2 + kz^2                    /
+    # convert input angles to radians
+    theta = np.deg2rad(theta)
+    phi = np.deg2rad(phi)
+    # generate the dipole kernel
+    assert (len(shape) == 3)
+    kk_x, kk_y, kk_z = pmu.coord(shape, origin)
+    dk_arr = (1.0 / 3.0 - (
+        kk_z * np.cos(theta) * np.cos(phi) - kk_y * np.sin(theta) * np.cos(phi)
+        + kk_x * np.sin(phi)) ** 2 / (kk_x ** 2 + kk_y ** 2 + kk_z ** 2))
+    # fix singularity at |k|^2 == 0 in the denominator
+    singularity = np.isnan(dk_arr)
+    dk_arr[singularity] = 1.0 / 3.0
+    return dk_arr
+
+
+W_proxy = np.arange(4,31).reshape(3,3,3)
+data_proxy = np.arange(20,47).reshape(3,3,3)
+convolution_proxy = np.arange(27).reshape(3,3,3)
+M_g_proxy = np.arange(2,29).reshape(3,3,3)
+chi_proxy = np.arange(50,77).reshape(3,3,3)
+
+
